d8ed6611db26686ec7e6cdb88c432a49b848b58a
Roman <roman.korotaev@alumni.esade.edu>
2018-12-03 11:08:16 +0100

    change3

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ app = dash.Dash(__name__)
     server = app.server
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
-
 available_indicators = df['NA_ITEM'].unique()
 countries = df['GEO'].unique()
 
